41f3c87f35adf27b1835ee5843aa29e972617f23
Navneet Prakash <hello_navneet@hotmail.com>
2019-03-19 17:44:01 +0530

    Adds description comments.

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,35 +1,53 @@
-function Blockchain () {
+"use strict";
+
+function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
 }
 
-Blockchain.prototype.createNewBlock = function (nonce, previousBlockHash, hash) {
+/**
+ * nonce: A number that comes from proof of work
+ * previousBlockHash:
+ * hash: All the transactions will be compressed into a single string that string will be hash.
+ */
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
     transations: this.pendingTransactions,
-    nonce: nonce,
-    hash: hash,
-    previousBlockHash: previousBlockHash
+    nonce,
+    hash,
+    previousBlockHash
   };
   this.pendingTransactions = [];
   this.chain.push(newBlock);
   return newBlock;
 };
 
-Blockchain.prototype.getLastBlock = function () {
-  if (this.chain.length)
-    return this.chain[this.chain.length - 1];
+Blockchain.prototype.getLastBlock = function() {
+  if (this.chain.length) return this.chain[this.chain.length - 1];
 };
 
-Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
+Blockchain.prototype.createNewTransaction = function(
+  amount,
+  sender,
+  recipient
+) {
   const newTransaction = {
-    amount: amount,
-    sender: sender,
-    recipient: recipient
+    amount,
+    sender,
+    recipient
   };
   this.pendingTransactions.push(newTransaction);
-  return this.getLastBlock()['index'] + 1;
+  return this.getLastBlock()["index"] + 1;
 };
 
 module.exports = Blockchain;
+
+/** Features of this BlockChain:
+ * Proof of work
+ * Mine new blocks
+ * Create transactions
+ * Validate the chain
+ * Retrieve address data
+ */
